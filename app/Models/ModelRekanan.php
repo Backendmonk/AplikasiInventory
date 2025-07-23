5f57f35f4c8fc96fd8e92bee0d4e8ea63733d56a
Backendmonk <59171380+Backendmonk@users.noreply.github.com>
2025-07-23 23:06:50 +0800

    updates

---
+++ app/Models/ModelRekanan.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelRekanan extends Model
+{
+    use HasFactory;
+
+    
+
+    protected $table = 'tb_rekanan';
+
+    protected $primaryKey = 'id';
+    protected $fillable  = ['id','nama_rekanan','alamat'];
+
+    public $timestamps = false;
+
+    public $incrementing = true;
+
+
+}
