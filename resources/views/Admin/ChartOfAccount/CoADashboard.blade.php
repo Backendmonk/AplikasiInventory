cd49740bafc0fd9757a0477c5f7380a1e4c70d34
Backendmonk <59171380+Backendmonk@users.noreply.github.com>
2025-11-02 11:19:08 +0800

    Update CoADashboard.blade.php

--- resources/views/Admin/ChartOfAccount/CoADashboard.blade.php
+++ resources/views/Admin/ChartOfAccount/CoADashboard.blade.php
@@ -104,7 +104,7 @@
  <br>
 <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Data Barang</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Data Chart Of Account</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
