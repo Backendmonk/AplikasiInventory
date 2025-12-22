fdc54aeb60e2052e48cce14df4999b0b2e782bdf
Backendmonk <59171380+Backendmonk@users.noreply.github.com>
2025-12-22 19:05:08 +0800

    Update pembelianbarang.blade.php

--- resources/views/Admin/Pembelian/pembelianbarang.blade.php
+++ resources/views/Admin/Pembelian/pembelianbarang.blade.php
@@ -151,8 +151,8 @@
                                                             <form action="/Admin/Pembelian/Detail" method="POST">
                                                               @csrf
                                                               <input type="text" hidden name="idnota" value="{{ $data->notaPembelian->id }}">
-
-                                                              {{-- @if ($data->notaPembelian->status_nota == 'Hutang')
+                                                              
+                                                              {{-- @if ($data->notaPembelian->status_nota == 'Hutang' test)
                                                               <li><button class="dropdown-item" type="submit" name ="pelunasan" value ="detail">Lunasi</button></li> --}}
                                                                   
                                                               
